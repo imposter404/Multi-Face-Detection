1e56fc58f2465a4d4e749b1a6d0261545937eb11
Imposter <ara404.github@gmail.com>
2025-05-17 21:53:57 +0530

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,11 @@ import matplotlib.pyplot as plt
 import PIL
 
 imagePath=""
+
+def Face_Detect():
+    # load image--------------------------
+    global imagePath
+
+    # read iamge--------------------------
+    img = cv2.imread(imagePath)
+    gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
