f65d192c861a70bad52a686ee499163eaa770997
Imposter <ara404.github@gmail.com>
2025-05-17 21:54:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -11,3 +11,7 @@ def Face_Detect():
     # read iamge--------------------------
     img = cv2.imread(imagePath)
     gray_image = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
+
+    # seclect the model --------------------------------
+    face_classifier = cv2.CascadeClassifier( 
+        cv2.data.haarcascades + "haarcascade_frontalface_default.xml" )
